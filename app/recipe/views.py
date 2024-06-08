219c8a7908ce4f4a955328743e465d85a9b08242
Nada Ayesh <nayesh10@students.iugaza.edu.ps>
2024-06-09 01:18:49 +0300

    make update delete

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -32,7 +32,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(
+                 mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
+
     serializer_class = TagSerializer
     queryset = Tag.objects.all()
     authentication_classes = [TokenAuthentication]
