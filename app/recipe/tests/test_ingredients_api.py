7f6a07826cea4d2e23ad2157c33b872372214063
Nada Ayesh <nayesh10@students.iugaza.edu.ps>
2024-06-09 06:50:47 +0300

    implement delete

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -68,3 +68,12 @@ class PrivateIngredientApiTests(TestCase):
 
         self.assertEqual(ingredient.name, payload['name'])
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_delete_ingredient(self):
+        ingredient = Ingredient.objects.create(user=self.user, name="Lattuce")
+
+        url = detail_url(ingredient.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Ingredient.objects.filter(user=self.user).exists())
